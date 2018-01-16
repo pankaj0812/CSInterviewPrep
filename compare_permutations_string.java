58bd6a5569ba3e6e178a98aed006f8fc25a6006b
Pooja Bhaumik <pbhaumik26@gmail.com>
2018-01-16 19:34:17 +0530

    Create compare_permutations_string.java

---
+++ compare_permutations_string.java
@@ -0,0 +1,28 @@
+// Java program to demonstrate difference between String,
+// StringBuilder and StringBuffer
+
+import java.util.*;
+class csInterview
+{
+
+	public static String sortString(String str)
+	{
+	    char tempArray[] = str.toCharArray();
+	    Arrays.sort(tempArray);
+	    return new String(tempArray);
+	}
+
+	public static void main(String[] args)
+	{
+	    String s1 = "ABCD" , s2 = "ADC";
+	    s1 = sortString(s1);
+	    s2 = sortString(s2);
+	    
+	    if(s1 == s2){
+	    System.out.println(s1 + " " + s2 );
+	    }
+	    else
+	    	    System.out.println("Not a permutation" );
+
+	}
+}
