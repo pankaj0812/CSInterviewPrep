732512a85a5a723a0089a7488bafb0aabab1447a
Pankaj Kumar Singh <singhpankajkumar65@gmail.com>
2018-01-27 20:51:15 +0530

    Create OneAway1-5.java
    Checking if two strings are one edit away or zero edits away.

---
+++ OneAway1-5.java
@@ -0,0 +1,79 @@
+/*package whatever //do not write package name here */
+/* condition for pales pale */
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args)
+	{
+	    boolean value;
+		String s1="pale",s2="ple";
+		s1 = s1.toLowerCase();s2 = s2.toLowerCase();
+		if (s1.equals(s2))
+		{
+		    value=true;
+            System.out.println(value);
+		}
+        else if (s1.length() == s2.length())
+		{
+		    value=equallength(s1,s2);
+		    System.out.println(value);
+		}
+		else if (s1.length()-s2.length()<=1)
+		{
+		    value=unequallength(s1,s2); 
+		    System.out.println(value);
+		}
+		else System.out.println(false);
+	}
+	public static boolean equallength(String s1, String s2)
+    {   
+        int count=0,flag=0;
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(s1);
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append(s2);
+        int len=s1.length(); 
+        for (int i=0;i<len;i++)
+            {
+                if (sb1.charAt(i) != sb2.charAt(i) && count<1) 
+                {
+                    sb2.setCharAt(i, sb1.charAt(i));
+                    count++;
+                 } 
+                 
+            }
+        if (sb1.toString().equals(sb2.toString()))
+        return true;
+        else
+        return false;
+    }
+    public static boolean unequallength(String s1, String s2)
+    {
+        int count=0,flag=0;
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(s1);
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append(s2);
+        //int len1=s1.length();int len2=s2.length();
+        int len=s1.length();
+        for (int i=0;i<len;i++)
+        {
+            if (sb1.charAt(i) != sb2.charAt(i) && count<1) 
+                {
+                    //System.out.println(sb1.charAt(i));
+                    //System.out.println(sb2.charAt(i));
+                    
+                    sb2.insert(i, sb1.charAt(i));
+                    count++;
+                    //System.out.println(sb1.charAt(i));
+                    //System.out.println(sb2.charAt(i));
+                    
+                 }
+                 
+        }
+        if (sb1.toString().equals(sb2.toString()))
+        return true;
+        else
+        return false;
+    }
+}
