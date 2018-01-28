3af966b896f609e97b0452c5d4416be3ebaeda1c
Pankaj Kumar Singh <singhpankajkumar65@gmail.com>
2018-01-28 22:42:53 +0530

    Create ContainsSubstring.java

---
+++ ContainsSubstring.java
@@ -0,0 +1,12 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		String s1="waterbottle";
+		String s2="erbottlewat";
+		String s3=s2+s2;
+		System.out.println(s3.contains(s1));
+	}
+}
